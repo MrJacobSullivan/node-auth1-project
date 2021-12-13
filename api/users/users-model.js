70eecdb3a42b891f7a7854b33805030769777ca9
mrjacobsullivan <mrjacobsullivan@gmail.com>
2021-12-13 15:31:53 -0600

    added users-model functions

--- api/users/users-model.js
+++ api/users/users-model.js
@@ -1,29 +1,34 @@
+const db = require('../../data/db-config')
+
 /**
   resolves to an ARRAY with all users, each user having { user_id, username }
  */
-function find() {
-
-}
+const find = () => db('users').select('user_id', 'username')
 
 /**
   resolves to an ARRAY with all users that match the filter condition
  */
-function findBy(filter) {
-
-}
+const findBy = (filter) => db('users').where(filter)
 
 /**
   resolves to the user { user_id, username } with the given user_id
  */
-function findById(user_id) {
-
-}
+const findById = (user_id) => db('users').where({ user_id }).first()
 
 /**
   resolves to the newly inserted user { user_id, username }
  */
-function add(user) {
+const add = async (user) => {
+  const [id] = await db('users').insert(user)
 
+  return findById(id)
 }
 
 // Don't forget to add these to the `exports` object so they can be required in other modules
+
+module.exports = {
+  find,
+  findBy,
+  findById,
+  add,
+}
