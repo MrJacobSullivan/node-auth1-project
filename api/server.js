14f2ea3d1080c87d6d4cb3305ebbbdad8cccbef8
mrjacobsullivan <mrjacobsullivan@gmail.com>
2021-12-13 15:44:16 -0600

    added session boilerplate to server.js

--- api/server.js
+++ api/server.js
@@ -1,35 +1,54 @@
-const express = require("express");
-const helmet = require("helmet");
-const cors = require("cors");
+const express = require('express')
+const session = require('express-session')
+const Store = require('connect-session-knex')(session)
+const helmet = require('helmet')
+const cors = require('cors')
 
-/**
-  Do what needs to be done to support sessions with the `express-session` package!
-  To respect users' privacy, do NOT send them a cookie unless they log in.
-  This is achieved by setting 'saveUninitialized' to false, and by not
-  changing the `req.session` object unless the user authenticates.
+const usersRouter = require('./users/users-router')
+const authRouter = require('./auth/auth-router')
 
-  Users that do authenticate should have a session persisted on the server,
-  and a cookie set on the client. The name of the cookie should be "chocolatechip".
+const server = express()
 
-  The session can be persisted in memory (would not be adecuate for production)
-  or you can use a session store like `connect-session-knex`.
- */
+server.use(helmet())
 
-const server = express();
+server.use(
+  session({
+    name: 'monkey',
+    secret: process.env.SESSION_SECRET || 'keep it secret, keep it safe',
+    cookie: {
+      maxAge: 1000 * 60 * 60,
+      secure: false,
+      httpOnly: false,
+    },
+    resave: false,
+    saveUninitialized: false,
+    rolling: true,
+    store: new Store({
+      knex: require('../database/db-config'),
+      tablename: 'sessions',
+      sidfieldname: 'sid',
+      createtable: true,
+      clearInterval: 1000 * 60 * 60,
+    }),
+  })
+)
 
-server.use(helmet());
-server.use(express.json());
-server.use(cors());
+server.use(express.json())
+server.use(cors())
 
-server.get("/", (req, res) => {
-  res.json({ api: "up" });
-});
+server.use('/api/users', usersRouter)
+server.use('/api/auth', authRouter)
 
-server.use((err, req, res, next) => { // eslint-disable-line
+server.get('/', (req, res) => {
+  res.json({ api: 'up' })
+})
+
+// eslint-disable-next-line
+server.use((err, req, res, next) => {
   res.status(err.status || 500).json({
     message: err.message,
     stack: err.stack,
-  });
-});
+  })
+})
 
-module.exports = server;
+module.exports = server
--- data/migrations/20191217174228_bootstrap-db.js
+++ data/migrations/20191217174228_bootstrap-db.js
@@ -1,16 +1,13 @@
 exports.up = function (knex) {
-  return knex.schema.createTable("users", users => {
-    users.increments("user_id");
+  return knex.schema.createTable('users', (users) => {
+    users.increments('user_id')
 
-    users
-      .string("username", 128)
-      .notNullable()
-      .unique();
+    users.string('username', 128).notNullable().unique()
 
-    users.string("password", 128).notNullable();
-  });
-};
+    users.string('password', 128).notNullable()
+  })
+}
 
 exports.down = function (knex) {
-  return knex.schema.dropTableIfExists("users");
-};
+  return knex.schema.dropTableIfExists('users')
+}
